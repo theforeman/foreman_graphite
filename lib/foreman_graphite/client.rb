3f2e8099b5791708809daca786d58c0d317b3785
Ohad Levy <ohadlevy@gmail.com>
2014-10-14 11:46:18 +0300

    change defaults not to stack up results.

--- lib/foreman_graphite/client.rb
+++ lib/foreman_graphite/client.rb
@@ -7,9 +7,9 @@ class ForemanGraphite::Client
     @client = GraphiteAPI.new(
       :graphite => (opts[:server] || "0.0.0.0:2003"), # required argument
       :prefix   => ['theforeman', fqdn], # add example.prefix to each key
-      :slice    => (opts[:slice] || 60), # results are aggregated in 60 seconds slices
-      :interval => (opts[:interval] || 60), # send to graphite every 60 seconds
-      :cache    => (opts[:cache] || 4 * 60 * 60) # set the max age in seconds for records reanimation
+      :slice    => (opts[:slice] || 0), # results are aggregated in 60 seconds slices
+      :interval => (opts[:interval] || 0), # send to graphite every 60 seconds
+      :cache    => (opts[:cache] || 0) # set the max age in seconds for records reanimation
     )
   end
 
